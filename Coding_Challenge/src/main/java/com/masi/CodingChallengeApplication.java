db8994e6314fab028ab60ab5f34652b69fd815f2
Sandeeprawat313 <sandeeprawat313@gmail.com>
2023-04-06 12:12:20 +0530

    updated pakage name

--- Coding_Challenge/src/main/java/com/masi/CodingChallengeApplication.java
+++ Coding_Challenge/src/main/java/com/masi/CodingChallengeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.masai.core.CorsFilter;
+import com.masi.core.CorsFilter;
 
 @SpringBootApplication
 public class CodingChallengeApplication {
